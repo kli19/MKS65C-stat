0a7231f4939113175eeaa1092def884c4cfe121a
Andy Zhou <azhou4@stuy.edu>
2018-10-30 15:36:03 -0400

    classwork

--- stat.c
+++ stat.c
@@ -4,6 +4,9 @@
 #include <sys/stat.h>
 
 int main(){
-
+  int file = open("file.txt", O_RDWR | O_CREAT, 0777);
+  struct stat *p = (struct stat *)malloc(10);
+  stat();
+  close(file);
   return 0;
 }
