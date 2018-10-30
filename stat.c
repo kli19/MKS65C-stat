831dd18a1dddf6dcc0d48ff20b73c78e8f58178c
0AndyZhou0 <azhou4@stuy.edu>
2018-10-30 18:42:03 -0400

    need to add correct time format

--- stat.c
+++ stat.c
@@ -6,8 +6,12 @@
 
 int main(){
   int file = open("file.txt", O_RDWR | O_CREAT, 0777);
+  write(file,"Hello\nBaka\n",11);
   struct stat p;
   stat("file.txt",&p);
+  printf("%ld\n", p.st_size);
+  printf("%d\n", p.st_mode);
+  printf("%ld\n", p.st_atime); // Set to correct format please
   printf("%ld\n", p.st_size);
   close(file);
   return 0;
