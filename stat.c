abc948cd9087c6a063cc9b60321045ccbdc6ed95
0AndyZhou0 <azhou4@stuy.edu>
2018-10-30 18:35:28 -0400

    fixed initial stat function

--- stat.c
+++ stat.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
 
 int main(){
   int file = open("file.txt", O_RDWR | O_CREAT, 0777);
-  struct stat *p = (struct stat *)malloc(10);
-  stat();
+  struct stat p;
+  stat("file.txt",&p);
   close(file);
   return 0;
 }
