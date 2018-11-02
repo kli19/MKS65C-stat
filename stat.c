2f9e6f6ba6a359f8f553b52ad13e91d97ab9d37f
Karen Li <kli19@stuy.edu>
2018-11-02 00:04:45 -0400

    permissions

--- stat.c
+++ stat.c
@@ -4,6 +4,26 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <string.h>
+
+char * permissify(int octal, char dest[]){  
+  char * p = dest;
+  char letters[9] = "rwxrwxrwx";
+  int i = 0;
+  while(octal){
+    if(octal % 2){
+      dest[8 - i] = letters[8 - i];
+    }
+    else{
+      dest[8 - i] = '-';
+    }
+    i++;
+    octal /= 2;   
+  }
+  return p;
+}
+
+
 
 int main(){
   int file = open("file.txt", O_RDWR | O_CREAT, 0777);
@@ -12,11 +32,14 @@ int main(){
   stat("file.txt",&p);
   printf("size: %ld\n", p.st_size);
   printf("mode: %d\n", p.st_mode);
-  printf("time: %s\n", ctime(&p.st_atime)); // Set to correct format please
+  printf("permissions in octal: %o\n", (int)(p.st_mode)%512);
+  char s[9];
+  printf("permissions: %s\n", permissify((p.st_mode)%512, s));
+  printf("time: %s", ctime(&p.st_atime)); 
   printf("size in bytes: %ld\n", p.st_size);
-  printf("size in kilobytes: %ld\n", p.st_size/1000);
-  printf("size in megabytes: %ld\n", p.st_size/1000000);
-  printf("size in gigabytes: %ld\n", p.st_size/1000000000);
+  printf("size in kilobytes: %lf\n", (float)p.st_size/1000);
+  printf("size in megabytes: %lf\n", (float)p.st_size/1000000);
+  printf("size in gigabytes: %lf\n", (float)p.st_size/1000000000);
   close(file);
   return 0;
 }
